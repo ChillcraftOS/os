5fd9e9da7fb80d4d46a21a54dea02cfc0794d752
oxmc <oxmc7769@gmail.com>
2022-05-01 18:58:57 -0400

    Update files

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -2,4 +2,5 @@
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
+        cp -r files/os-release /usr/lib/os-release
 EOF
--- stage4/06-custom/00-run.sh
+++ stage4/06-custom/00-run.sh
@@ -4,9 +4,14 @@ on_chroot << EOF
 #Splash screen
 git clone https://github.com/snorain/splashscreen
 cd splashscreen
+ls
 bash install
+cd ..
+rm -rf splashscreen
 #Startup sound
-git clone https://github.com/QuintOS/startupsound
-cd satrtupsound
+git clone https://github.com/snorain/startupsound
+cd startupsound
 bash install
+cd ..
+rm -rf startupsound
 EOF
