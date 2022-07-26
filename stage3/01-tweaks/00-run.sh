2bbfaf01b61c2649daa3c88af6ee203cf7796277
oxmc <67136658+oxmc@users.noreply.github.com>
2022-07-25 22:50:14 -0700

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -3,4 +3,5 @@
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
         cp -r files/os-release /usr/lib/os-release
+	cp -r files/pi-greeter.conf /etc/lightdm/pi-greeter.conf
 EOF
