d8934f3a658769be7b869908b2b595c357f495ff
oxmc <67136658+oxmc@users.noreply.github.com>
2022-07-27 16:14:24 -0700

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -4,4 +4,7 @@ on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
         cp -r files/os-release /usr/lib/os-release
 	cp -r files/pi-greeter.conf /etc/lightdm/pi-greeter.conf
+	cp -r files/asplashscreen /etc/init.d/asplashscreen
+	sudo chmod a+x /etc/init.d/asplashscreen
+        sudo insserv /etc/init.d/asplashscreen
 EOF
