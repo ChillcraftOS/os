5b0d894154d4f0a36033780e81fd940d689afa6f
oxmc <67136658+oxmc@users.noreply.github.com>
2022-07-27 16:27:12 -0700

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -8,4 +8,6 @@ on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" chmod a+x /etc/init.d/asplashscreen
 	echo "Not enabling splash service, to enable it run: sudo insserv /etc/init.d/asplashscreen"
         #SUDO_USER="${FIRST_USER_NAME}" insserv /etc/init.d/asplashscreen
+	#devicee noises
+	install -v -o 1000 -g 1000 -m 644 "files/device-noise.rule" "${ROOTFS_DIR}/etc/udev/rules.d/71-device-conn-diss.rules"
 EOF
