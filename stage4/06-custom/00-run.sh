c5a33c180fd7bdf610a085423e9f95effc538ac9
oxmc <67136658+oxmc@users.noreply.github.com>
2022-07-27 16:26:33 -0700

    Update 00-run.sh

--- stage4/06-custom/00-run.sh
+++ stage4/06-custom/00-run.sh
@@ -13,6 +13,6 @@ cd startupsound
 bash install
 cd ..
 rm -rf startupsound
-EOF
 #devicee noises
-install -v -o 1000 -g 1000 -m 644 "files/device-noise.rules" "${ROOTFS_DIR}/etc/udev/rules.d/71-device-conn-diss.rules"
+#install -v -o 1000 -g 1000 -m 644 "files/device-noise.rules" "${ROOTFS_DIR}/etc/udev/rules.d/71-device-conn-diss.rules"
+EOF
