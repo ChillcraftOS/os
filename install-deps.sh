f354016b4920c5107d11c91e1bd8667cfad1456f
oxmc <67136658+oxmc@users.noreply.github.com>
2022-07-27 19:50:57 -0700

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,3 @@
 sudo apt install -y coreutils quilt parted qemu-user-static debootstrap zerofree zip \
 dosfstools libarchive-tools libcap2-bin grep rsync xz-utils file git curl bc \
-qemu-utils kpartx gpg pigz
+qemu-utils kpartx gpg pigz kmod
