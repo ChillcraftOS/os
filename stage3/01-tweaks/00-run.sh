84a7823b153becc67371dd21ffb468dc0f2ac254
oxmc <67136658+oxmc@users.noreply.github.com>
2023-01-16 01:22:30 -0800

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -6,7 +6,7 @@ ls ../
 install -m 644 ../files/os-release "${ROOTFS_DIR}/usr/lib/os-release"
 install -m 644 ../files/pi-greeter.conf "${ROOTFS_DIR}/etc/lightdm/pi-greeter.conf"
 install -m 644 ../files/asplashscreen "${ROOTFS_DIR}/etc/init.d/asplashscreen"
-install -m 644 ../files/device-noise.rule "${ROOTFS_DIR}/etc/udev/rules.d/71-device-conn-diss.rules"
+install -m 644 ../files/device-noises.rule "${ROOTFS_DIR}/etc/udev/rules.d/71-device-conn-diss.rules"
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
