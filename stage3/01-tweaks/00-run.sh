bd07357d99386fe3ca88e591f6a5739a3088b611
oxmc <67136658+oxmc@users.noreply.github.com>
2023-01-15 23:06:10 -0800

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
+install -m 644 files/os-release "${ROOTFS_DIR}/usr/lib/os-release"
+install -m 644 files/pi-greeter.conf "${ROOTFS_DIR}/etc/lightdm/pi-greeter.conf"
+install -m 644 files/asplashscreen "${ROOTFS_DIR}/etc/init.d/asplashscreen"
+
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
-	ls
-        cp -r files/os-release /usr/lib/os-release
-	cp -r files/pi-greeter.conf /etc/lightdm/pi-greeter.conf
-	cp -r files/asplashscreen /etc/init.d/asplashscreen
 	SUDO_USER="${FIRST_USER_NAME}" chmod a+x /etc/init.d/asplashscreen
 	echo "Not enabling splash service, to enable it run: sudo insserv /etc/init.d/asplashscreen"
         #SUDO_USER="${FIRST_USER_NAME}" insserv /etc/init.d/asplashscreen
