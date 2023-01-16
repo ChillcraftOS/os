57e36103a4d750ee53bcf863e1751db195028184
oxmc <67136658+oxmc@users.noreply.github.com>
2023-01-15 23:52:34 -0800

    Update 00-run.sh

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
-install -m 644 files/os-release "${ROOTFS_DIR}/usr/lib/os-release"
-install -m 644 files/pi-greeter.conf "${ROOTFS_DIR}/etc/lightdm/pi-greeter.conf"
-install -m 644 files/asplashscreen "${ROOTFS_DIR}/etc/init.d/asplashscreen"
+ls
+
+install -m 644 ../files/os-release "${ROOTFS_DIR}/usr/lib/os-release"
+install -m 644 ../files/pi-greeter.conf "${ROOTFS_DIR}/etc/lightdm/pi-greeter.conf"
+install -m 644 ../files/asplashscreen "${ROOTFS_DIR}/etc/init.d/asplashscreen"
 
 on_chroot << EOF
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
